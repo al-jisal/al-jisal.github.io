bf70a0fa23be010878a52a48c162e214071555ad
al_jisal <aljisal.frimpong@gmail.com>
2025-06-01 23:57:11 -0400

    almost done with the hero section

---
+++ Portfolio Site/src/sections/Hero.jsx
@@ -0,0 +1,58 @@
+import { Canvas } from '@react-three/fiber';
+import { PerspectiveCamera } from '@react-three/drei';
+import HackerRoom from '../components/HackerRoom';
+import { Suspense } from 'react';
+import CanvasLoader from '../components/CanvasLoader';
+import { Leva } from 'leva';
+import { useControls } from 'leva';
+
+const Hero = () => {
+    const x =  useControls({ schemaOrFolderName: 'HackerRoom', settingsOrDepsOrSchema: {
+        positionX: {
+            value: 2.5,
+            min: -10,
+            max: 10
+        },
+        positionY: {
+            value: 2.5,
+            min: -10,
+            max: 10
+        },
+        positionZ: {
+            value: 2.5,
+            min: -10,
+            max: 10
+        }
+    }})
+
+    return (
+        <section className="min-h-screen w-full flex flex-col relative">
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+                    Hi, I am Desmond <span className="waving-hand">👋</span>
+                </p>
+                <p className="hero_tag text-gray_gradient">
+                    Building Digital Footprints
+                </p>
+            </div>
+
+            <div className="w-full h-full absolute inset-0">
+                <Leva />
+                <Canvas className="w-full h-full">
+                    <Suspense fallback={<CanvasLoader />}>
+                        <PerspectiveCamera makeDefault position={[0, 0, 30]}/>
+                        <HackerRoom 
+                            position={[0, 0, 0]}
+                            rotation={[0, 280, 0]}
+                            scale={[x.positionX, x.positionY, x.positionZ]}
+                        />
+
+                        <ambientLight intensity={1} />
+                        <directionalLight position={[10, 10, 10]} intensity={0.5} />
+                    </Suspense>
+                </Canvas>
+            </div>
+        </section>
+    )
+}
+export default Hero;
