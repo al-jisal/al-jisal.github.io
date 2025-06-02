3ad2791fd583e08ba10e81e130e52773a7a1dc89
al_jisal <aljisal.frimpong@gmail.com>
2025-06-01 21:16:28 -0400

    added a Navbar section

--- Portfolio Site/src/App.jsx
+++ Portfolio Site/src/App.jsx
@@ -1,10 +1,6 @@
-import React from 'react';
-
 const App = () => {
   return (
-    <main className="max-w-7xl mx-auto">
-      <h1 className="text-2xl text-white underline">Hello, Three.js</h1>
-    </main>
+    <div className="text-white">Navbar</div>
   )
 }
-export default App;
+export default App
---
+++ Portfolio Site/src/sections/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <main className="max-w-7xl mx-auto">
+            <Navbar />
+        </main>
+    )
+}
+export default Navbar;
