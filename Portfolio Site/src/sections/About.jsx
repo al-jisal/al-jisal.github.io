23b1ea389e7f6b537548e8d96d6d183784506edc
al_jisal <aljisal.frimpong@gmail.com>
2025-06-07 14:09:52 -0400

    completed the About section

--- Portfolio Site/src/sections/About.jsx
+++ Portfolio Site/src/sections/About.jsx
@@ -1,4 +1,17 @@
-const About = () => {
+import Globe from "react-globe.gl";
+import Button from "../components/Button";
+import { useState } from "react";
+
+const About = () => { 
+    const [hasCopied, setHasCopied] = useState(false);
+
+    const handleCopy = () => {
+       navigator.clipboard.writeText("desmond.frimpong@colby.edu");
+       setHasCopied(true);
+       setTimeout(() => {
+              setHasCopied(false);
+            }, 2000);
+    }
     
     return (
         <section className="c-space my-20">
@@ -33,8 +46,55 @@ const About = () => {
                 <div className="col-span-1 xl:row-span-4">
                     <div className="grid-container">
                         <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
-                            
+                            <Globe 
+                                height={326}
+                                width={326}
+                                backgroundColor="rgba(0, 0, 0, 0)"
+                                backgroundImageOpacity={0.5}
+                                showAtmosphere
+                                showGraticules
+                                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                                pointsData={[
+                                    { lat: 4.88447, lng: -1.75536, size: 0.5, color: 'red' }
+                                ]}
+                            />
                         </div>
+                        <div>
+                            <p className="grid-headtext">Where I am</p>
+                            <p className="grid-subtext">
+                                I am based in Waterville, US. I am open to remote work opportunities 
+                                and willing to relocate for the right opportunity.
+                            </p>
+                            <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="xl:col-span-2 xl:row-span-3">
+                    <div className="grid-container">
+                        <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain"/>
+                        <div>
+                            <p className="grid-headtext">My Passion for Algorithmic Thinking</p>
+                            <p className="grid-subtext">I love solving problems and building things through code. Programming isn't just my profession:
+                                it's my way of contributing to the world.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="xl:col-span-1 xl:row-span-2">
+                    <div className="grid-container">
+                        <img src="/assets/grid4.png" alt="grid-4" className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"/>
+                        <di className="space-y-2">
+                            <p className="grid-subtext text-center">Contact Me</p>
+                            <div className="copy-container" onClick={handleCopy}>
+                                <img src={hasCopied ? '/assets/tick.svg' : '/assets/copy.svg'} alt="copy" />
+                                <p className="grid-subtext">
+                                    desmond.frimpong@gmail.com
+                                </p>
+                            </div>
+                        </di>
                     </div>
                 </div>
             </div>
